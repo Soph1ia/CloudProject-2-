87172c1a16a8a4cf530a106aca1d2933d3ee728c
Soph1ia <37844553+Soph1ia@users.noreply.github.com>
2019-01-21 21:25:22 +0000

    merging with secondary file from base branch

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: 'Cloud | Home'
+    });
 });
-
+/*Renders interface page*/
+router.get('/interfacePage', function(req, res, next) {
+  res.render('interfacePage', { title: 'Express' });
+});
+/*Renders aboutUs page*/
+router.get('/aboutUs', function(req, res, next) {
+  res.render('aboutUs', { title: 'Express' });
+});
 module.exports = router;
